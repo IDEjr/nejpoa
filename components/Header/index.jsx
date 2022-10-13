5a85c0f91f384a7cf4c57d30239b3a405265ef2e
igordelatorre <delatorreigor0@gmail.com>
2022-10-13 20:13:37 -0300

    header init

---
+++ components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./style.modules.css";
+
+export default function Header() {
+  return (
+    <>
+      <h2 className={styles.header}>Header</h2>
+    </>
+  );
+}
