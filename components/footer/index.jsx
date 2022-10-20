245bf2938ce7bde2411ea79ae673dff122a16b5c
Renato Silveira <renpoa@gmail.com>
2022-10-20 16:34:20 -0300

    ajustes no footer #14

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,10 +1,24 @@
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 export default function footer(props)
 {
 
     return(
-        <div>Teste</div>
+
+        <div className={styles.box}>
+            <div className={styles.logo}>
+                <Image src={'/footer/logo.png'} width={'200vw'} height={'75vh'}/>
+            </div>
+            <div className={styles.info}>
+                <Image src={'/footer/Instagram.png'} width={'20%'} height={'20%'}/>
+                <span className={styles.text}>@nejpoa</span>
+            </div>
+            <div className={styles.info}>
+                <Image src={'/footer/Instagram.png'} width={'20%'} height={'20%'}/>
+                <span className={styles.text}>@nejpoa</span>
+            </div>
+        </div>
     )
 }
 
