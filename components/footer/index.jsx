5987419d71d3815a1a1501dd544ed2ab224674df
Spolavore <75754941+Spolavore@users.noreply.github.com>
2022-10-23 14:32:40 -0300

    footer recebendo email de contato e @ do instagram#14

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,22 +1,22 @@
 import styles from './footer.module.css'
 import Image from 'next/image'
 
-export default function footer(props)
+export default function footer({instagram, email})
 {
-
+    
     return(
 
         <div className={styles.box}>
             <div className={styles.logo}>
-                <Image src={'/footer/logo.png'} width={'200vw'} height={'75vh'}/>
+                <Image src={'/footer/logo_nej.svg'} width={'200vw'} height={'75vh'}/>
             </div>
             <div className={styles.info}>
-                <Image src={'/footer/Instagram.png'} width={'20%'} height={'20%'}/>
-                <span className={styles.text}>@nejpoa</span>
+                <Image src={'/footer/Instagram.svg'} width={'20%'} height={'20%'}/>
+                <span className={styles.text}>{instagram}</span>
             </div>
             <div className={styles.info}>
-                <Image src={'/footer/Instagram.png'} width={'20%'} height={'20%'}/>
-                <span className={styles.text}>@nejpoa</span>
+                <Image src={'/footer/Icon.svg'} width={'20%'} height={'20%'}/>
+                <span className={styles.text}>{email}</span>
             </div>
         </div>
     )
