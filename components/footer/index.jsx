6b918521ac2bfd005984dd0e16b271aeb2916049
Spolavore <75754941+Spolavore@users.noreply.github.com>
2022-10-23 17:45:53 -0300

    alteracoes no logo footer #14

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -7,16 +7,17 @@ export default function footer({instagram, email})
     return(
 
         <div className={styles.box}>
-            <div className={styles.logo}>
-                <Image src={'/footer/logo_nej.svg'} width={'200vw'} height={'75vh'}/>
-            </div>
+                <div className={styles.logo}>
+                    <Image src={'/footer/logo_nej.svg'} width={'300vw'} height={'100%'}/>
+                    <span className={styles.text_logo}>Conectando propósitos</span>
+            </div> 
             <div className={styles.info}>
                 <Image src={'/footer/Instagram.svg'} width={'20%'} height={'20%'}/>
-                <span className={styles.text}>{instagram}</span>
+                <span className={styles.text_icons}>{instagram}</span>
             </div>
             <div className={styles.info}>
                 <Image src={'/footer/Icon.svg'} width={'20%'} height={'20%'}/>
-                <span className={styles.text}>{email}</span>
+                <span className={styles.text_icons}>{email}</span>
             </div>
         </div>
     )
