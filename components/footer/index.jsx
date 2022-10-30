10e9e0a85035bbee675f57aa8ddabcb86d8b1992
Spolavore <75754941+Spolavore@users.noreply.github.com>
2022-10-30 12:59:45 -0300

    modificacoes no footer

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -7,17 +7,22 @@ export default function footer({instagram, email})
     return(
 
         <div className={styles.box}>
-                <div className={styles.logo}>
-                    <Image src={'/footer/logo_nej.svg'} layout="fill"/>
+                <div className={styles.box_content}>
+                   <div className={styles.logo}>
+                    <Image src={'/footer/logo_nej.svg'} width={"200vw"} height={"75vh"}/>
                     <span className={styles.text_logo}>Conectando propósitos</span>
+                   </div>
+                <div className={styles.info}>
+                    <Image src={'/footer/Instagram.svg'} width={'20%'} height={'20%'}/>
+                    <span className={styles.text_icons}>{instagram}</span>
+                </div>
+                <div className={styles.info}>
+                    <Image src={'/footer/Icon.svg'} width={'20%'} height={'20%'}/>
+                    <span className={styles.text_icons}>{email}</span>
+                </div>  
             </div> 
-            <div className={styles.info}>
-                <Image src={'/footer/Instagram.svg'} width={'20%'} height={'20%'}/>
-                <span className={styles.text_icons}>{instagram}</span>
-            </div>
-            <div className={styles.info}>
-                <Image src={'/footer/Icon.svg'} width={'20%'} height={'20%'}/>
-                <span className={styles.text_icons}>{email}</span>
+            <div className={styles.box_CNPJ}>
+                <span className={styles.text_CNPJ}>2022 | Núcleo das empresas Juniores de Porto Alegre CNPJ: 30.782.475/0001-48</span>
             </div>
         </div>
     )
