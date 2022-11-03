c893ad781373d668cd2a23f678eb3dba0ea9736b
Spolavore <75754941+Spolavore@users.noreply.github.com>
2022-11-03 12:34:41 -0300

    modularização dos arquivos css #14

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -1,4 +1,7 @@
-import styles from './footer.module.css'
+import styles from './css/content.module.css'
+import boxStyles from './css/boxes.module.css'
+import textStyles from './css/text.module.css'
+import imageStyles from './css/images.module.css'
 import Image from 'next/image'
 
 export default function footer({instagram, email})
@@ -6,23 +9,23 @@ export default function footer({instagram, email})
     
     return(
 
-        <div className={styles.box}>
-                <div className={styles.box_content}>
+        <div className={boxStyles.box}>
+                <div className={boxStyles.box_content}>
                    <div className={styles.logo}>
-                    <div className={styles.image_logo}>
+                    <div className={imageStyles.image_logo}>
                         <Image src={'/footer/logo_nej.svg'} layout= {'fill'}/>
                     </div>
-                    <span className={styles.text_logo}>Conectando propósitos</span>
+                    <span className={textStyles.text_logo}>Conectando propósitos</span>
                    </div>
                 <div className={styles.info}>  
                     <Image src={'/footer/Instagram.svg'} width={'30%'} height={'30%'}/>
-                    <span className={styles.text_icons}>{instagram}</span>
+                    <span className={textStyles.text_icons}>{instagram}</span>
                     <Image src={'/footer/Icon.svg'} width={'30%'} height={'30%'}/>
-                    <span className={styles.text_icons}>{email}</span>
+                    <span className={textStyles.text_icons}>{email}</span>
                 </div>  
             </div> 
-            <div className={styles.box_CNPJ}>
-                <span className={styles.text_CNPJ}>2022 | Núcleo das empresas Juniores de Porto Alegre CNPJ: 30.782.475/0001-48</span>
+            <div className={boxStyles.box_CNPJ}>
+                <span className={textStyles.text_CNPJ}>2022 | Núcleo das empresas Juniores de Porto Alegre CNPJ: 30.782.475/0001-48</span>
             </div>
         </div>
     )
