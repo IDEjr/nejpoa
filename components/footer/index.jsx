2dac59177769ab25fef12f7ee976b57ef3d965e3
Spolavore <75754941+Spolavore@users.noreply.github.com>
2022-11-03 20:48:21 -0300

    finalização do footer xD

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -3,6 +3,7 @@ import boxStyles from './css/boxes.module.css'
 import textStyles from './css/text.module.css'
 import imageStyles from './css/images.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function footer({instagram, email})
 {
@@ -18,10 +19,18 @@ export default function footer({instagram, email})
                     <span className={textStyles.text_logo}>Conectando propósitos</span>
                    </div>
                 <div className={styles.info}>
-                    <Image src={'/footer/Instagram.svg'} width={'35%'} height={'35%'}/>
+                  <a className={styles.link_insta} href='https://www.instagram.com/nejpoa/'>
+                    <div className={imageStyles.icon_insta}>
+                        <Image src={'/footer/Instagram.svg'} layout={'fill'}/>
+                    </div>    
                     <span className={textStyles.text_icons}>{instagram}</span>
-                    <Image src={'/footer/Icon.svg'} width={'35%'} height={'35%'}/>
+                  </a>
+                  <a className = {styles.link_email} href='mailto: contato@nejpoa.com.br'>
+                    <div className={imageStyles.icon_email}>
+                        <Image src={'/footer/Icon.svg'} layout={'fill'}/>
+                    </div>
                     <span className={textStyles.text_icons}>{email}</span>
+                  </a>  
                 </div>  
             </div> 
             <div className={boxStyles.box_CNPJ}>
