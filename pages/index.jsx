44a2277095a0ff6ca6c0dc14b1fd09182c860003
Spolavore <75754941+Spolavore@users.noreply.github.com>
2022-11-27 11:56:33 -0300

    first commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Script from "next/script";
 import Header from "../components/Header";
+import MEJ from "../components/MEJ";
 import { handleJSONfiles } from "../functions/jsonHandler";
 
 export function getStaticProps() {
