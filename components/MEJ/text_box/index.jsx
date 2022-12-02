07e15d04d9ffb2de057fef1637ff3b48e2fbf3f6
Spolavore <75754941+Spolavore@users.noreply.github.com>
2022-12-01 22:59:55 -0300

    ajustes

--- components/MEJ/text_box/index.jsx
+++
@@ -1,9 +0,0 @@
-import style from './style.module.css'
-export default function TextBox(props){
-    return(
-        <div className={style.text_box}>
-            <h1>{props.titulo}</h1>
-            <span>{props.conteudo}</span>
-        </div>
-    )
-}
