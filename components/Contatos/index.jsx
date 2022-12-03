8b104f5099cdd0593e8421456e4dd6af1f5c6c40
Carol Bernich <carolinebernich@hotmail.com>
2022-12-03 14:40:26 -0300

    commit shadows

--- components/Contatos/index.jsx
+++ components/Contatos/index.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import style from "./contato.module.css";
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import Modal from 'react-modal';
-// import emailjs from '@emailjs/browser';
-
-
-
+// import { BsArrowRight } from 'react-icons/fa';
 
 const customStyles = {
   content : {
@@ -18,27 +14,9 @@ const customStyles = {
       transform             : 'translate(-50%, -50%)'
   }};
 
+
 export default function Contacts() {
   
-  
-
-    // const form = useRef();
-
-    // const [modalIsOpen,setIsOpen] = React.useState(false);
-    // function openModal() {
-    //     setIsOpen(true);
-    // }
-    // var subtitle;
-
-    // function afterOpenModal() {
-    // // references are now sync'd and can be accessed.
-    //     subtitle.style.color = '#3f7f92';
-    // }
-    
-    // function closeModal(){
-    //     setIsOpen(false);
-    // }
-
     const [inputs, setInputs] = useState({});
     const [textarea, setTextarea] = useState('');
     
@@ -50,24 +28,15 @@ export default function Contacts() {
       setTextarea(values => ({...values, [name]: value}))
     }
   
-    // const handleSubmit = (event) => {
-      
-    //   // event.preventDefault();
-
-    // alert("Email enviado");
-    // }
-  
     return ( 
       
     <div className={style.container}>
         <div className={style.contact_text}>
           <h1 className={style.h1}>Contato</h1>
-          {/* <image src="arrow-right.svg" alt="" className="imgArrow"/> */}
-          <h3 className={style.h3}>Se interessou em ser parceiro do NEJ POA? Quer entender melhor como uma Empresa Júnior pode resolver a dor da sua empresa? Ou quer criar uma EJ? Entre em contato que te responderemos em até 2 dias úteis.</h3>
+          
+        <h3 className={style.h3}>  Se interessou em ser parceiro do NEJ POA? Quer entender melhor como uma Empresa Júnior pode resolver a dor da sua empresa? Ou quer criar uma EJ? Entre em contato que te responderemos em até 2 dias úteis.</h3>
+          
       </div>
-      
-    
-   
       <div className={style.contact_forms}>
       <form id="form1" className={style.form} action="https://formsubmit.co/contato@nejpoa.com.br" target = "_blank"
           method="POST">
@@ -115,9 +84,6 @@ export default function Contacts() {
           <input className={style.btn_submit}
            type="submit"
            placeholder="Enviar"
-           
-           
-           
             />
       </form>
       </div>
