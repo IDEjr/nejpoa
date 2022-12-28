4aadf9e986279d4b85d22a1201557cf4d380d7b6
ArthurEly <arthur.ely78@gmail.com>
2022-12-28 02:00:55 -0300

    criação e estilização do MVV
    no momento, falta refatorar o css e adicionar as imagens

--- components/MVV/OurRules/index.jsx
+++ components/MVV/OurRules/index.jsx
@@ -1,9 +1,14 @@
-import style from './styles.module.css'
+import style from "../styles.module.css"
 
-export default function OurMission(){
+export default function OurRules(){
     return (
-        <div className={style.container}>
-            rulado
+        <div className={style.mvv}>
+            <div className={style.title}>
+                Nossas RNNS
+            </div>
+            <div className={style.content}>
+                ruladex
+            </div> 
         </div>
     )
 }
--- components/MVV/index.jsx
+++ components/MVV/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import style from './styles.module.css'
 import OurMission from './OurMission';
-import OurValues from './OurValues';
+import MVVBox from './MVVBox';
 import OurRules from './OurRules';
 
 export default function MVV(){
@@ -11,6 +11,21 @@ export default function MVV(){
         {value: "valores", text: "Valores"},  
         {value: "regras", text: "Regras Não Negociáveis"}, 
     ]
+
+    const ourValues = [
+        {img:"", title: "Elã", description: "Somos impulso. Um movimento impetuoso, afetuoso e expansivo. Temos entusiasmo criador. Agimos com emoção, calor e vivacidade. Somos inspiração."},
+        {img:"", title: "Manada", description: "Reconhecemos que a união faz a força! Trabalhamos em um espaço seguro, somos transparentes, vulneráveis e nos damos feedbacks sinceros. Somos acolhedores e cuidamos uns dos outros, nos aplaudimos, pedimos ajuda e compartilhamos nossas vitórias e derrotas. Somos timão."},
+        {img:"", title: "Visão Sistêmica", description: "Somos impulso. Um movimento impetuoso, afetuoso e expansivo. Temos entusiasmo criador. Agimos com emoção, calor e vivacidade. Somos inspiração."},
+        {img:"", title: "Liderar pelo exemplo", description: "Somos quem queremos ver. Buscamos constantemente ser reflexo e referência pra nossa rede. Tomamos a frente quando necessário, construindo e sendo responsáveis pelos nossos resultados."},
+    ]
+
+    const ourRules = [
+        {img:"", title: "Trabalhamos pelos melhores resultados", description: "Nossa régua é alta. Buscamos compreender o que fazemos e entregar o melhor para a nossa rede, queremos ser especialistas, inovadores e ousados."},
+        {img:"", title: "Aprendemos com a nossa história", description: "Reconhecemos que outros vieram antes de nós e que estamos aqui porque nossa historia está sendo escrita faz tempo. Compreendemos nosso passado enquanto organização e tomamos decisões baseadas no que já vivemos. Seguimos escrevendo nossa história com coerência e preparamos os próximos atores da organização."},
+        {img:"", title: "Buscamos ter equilibrio", description: "Compreendemos nossos desafios e limites, e damos nosso melhor, temos a consciencia de que precisamos cuidar do nosso bem-estar e do da rede para chegar aonde queremos."},
+        {img:"", title: "Não deixamos a peteca cair", description: "Trabalhamos e acreditamos desde o início. Todos somos responsáveis pelo alto impacto da nossa rede, lutamos por isso até o final e ninguém fica pra trás."},
+    ]
+
     return (
         <div className={style.container}>
             <div className={style.optionsContainer}>
@@ -31,9 +46,9 @@ export default function MVV(){
                 {
                     selectedMvv === "missao" ?  <OurMission/> 
                     :
-                    selectedMvv === "valores" ? <OurValues/>
+                    selectedMvv === "valores" ? <MVVBox title="Nosso valores" json={ourValues}/>
                     :
-                    /*sobra o outro*/           <OurRules/>
+                    /*sobra o outro*/           <MVVBox title="Nossos RNNS" json={ourRules}/>
                 }
             </div> 
         </div>
